5f6e349c9f42e148917f460dfcaf1b4a47371426
Repaka <845032@cognizant.com>
2021-05-09 12:54:20 +0530

    first commit

---
+++ TweetAppSolution/TweetAPP/TweetAPP/Migrations/20210501074451_tweet.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TweetAPP.Migrations
+{
+    public partial class tweet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Tweets_TweetId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TweetId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Tweets_TweetId",
+                table: "Comments",
+                column: "TweetId",
+                principalTable: "Tweets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Tweets_TweetId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TweetId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Tweets_TweetId",
+                table: "Comments",
+                column: "TweetId",
+                principalTable: "Tweets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
